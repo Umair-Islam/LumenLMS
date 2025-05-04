d369e37d8b166fd8e3ff605152bbd320cde5b1e8
Umair-Islam <umairislamwadi@gmail.com>
2025-05-04 05:33:52 +0000

    Firebase sdk

--- backend/config/firebase-admin.js
+++ backend/config/firebase-admin.js
@@ -1,6 +1,6 @@
 // backend/config/firebase-admin.js
 const admin = require('firebase-admin');
-const serviceAccount = require('../path-to-your-firebase-admin-sdk.json'); // Add your Firebase admin SDK file here
+const serviceAccount = require('/workspaces/LumenLMS/backend/config/lumen-a9ef2-firebase-adminsdk-fbsvc-f77fd4727b.json'); // Add your Firebase admin SDK file here
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
